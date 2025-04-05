a627a7126dafd046decf2683b7d24f77e5032b56
Mwadali <wellingtonmwadali@gmail.com>
2025-04-05 19:45:56 +0300

    pricing config files

--- client/src/garagePricing.tsx
+++ client/src/garagePricing.tsx
@@ -1,5 +1,5 @@
 import  { useState } from 'react';
-import logo from './assets/logo.png';
+import logo from './assets/Hipposautospa-logo.png';
 
 const categories = ['Colour & Repair', 'Clean & Shine', 'Coat & Guard'] as const;
 
